3ed0d8c3d06d1d700904464414f2fab17fa4bc9a
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2025-06-01 00:59:06 +0200

    add contactforms and css

--- web/themes/custom/mytheme/js/script.js
+++ web/themes/custom/mytheme/js/script.js
@@ -1,18 +1,15 @@
 (function (Drupal, once) {
   Drupal.behaviors.myThemeBehavior = {
     attach(context, settings) {
+
       once('myThemeSmoothScroll', 'a[href^="#"]', context).forEach(function (anchor) {
         anchor.addEventListener('click', function (e) {
           const href = this.getAttribute('href');
-
           if (href.length > 1 && href.startsWith('#')) {
             const target = document.querySelector(href);
             if (target) {
               e.preventDefault();
-              target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-              });
+              target.scrollIntoView({ behavior: 'smooth', block: 'start' });
               target.setAttribute('tabindex', '-1');
               target.focus();
             }
@@ -20,7 +17,8 @@
         });
       });
 
-      console.log('🎉 mytheme JavaScript loaded!');
+
+      console.log("🎉 mytheme JavaScript loaded!");
       document.body.style.borderTop = "5px solid salmon";
     }
   };
