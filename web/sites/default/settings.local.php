cc97c1931d7c48ff92b16c1ded8770368f4747e7
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2025-06-01 23:31:48 +0200

    Force commit Twig debug config files from web/sites/default

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,10 @@
+if (file_exists($app_root . '/' . $site_path . '/services.local.yml')) {
+  $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
+}
+
+$settings['twig_debug'] = TRUE;
+$settings['rebuild_access'] = TRUE;
+$settings['cache'] = FALSE;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
